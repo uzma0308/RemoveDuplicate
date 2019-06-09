f53e30a6762aea748b908e94695f95d250b1fac9
uzma ahmed <uzmaahmed.12@gmail.com>
2019-06-09 10:32:15 -0400

    chng

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
 const app = express();
-const path = require('path');
+const PORT = process.env.PORT || 80
 const fs = require('fs');
 var url = require('url') ;
-const cool = require('cool-ascii-faces')
 const router = express.Router();
 const bodyParser = require("body-parser");
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;  
@@ -25,21 +25,56 @@ router.get('/',function(req,res){
   var hostname = req.headers.host; 
   var pathname = url.parse(req.url).pathname; 
   console.log('http://' + hostname + pathname);
-
-  res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 router.post('/getPageData',function(req,res){
 
-  fs.readFile('./public/inspirationalImage2.csv', 'utf8', function(err, data)
+  fs.readFile('./public/data.csv', 'utf8', function(err, data)
   {
     console.log(req.body.page);
     var pagenumber = req.body.page;
       if (err)
           console.log(err);
       let lines = data.split('\n');
-      var responseData = lines.slice((pagenumber-1)*10,pagenumber*10);
-      res.json({ data: responseData });
+      var responseData = lines.slice((pagenumber-1)*500,pagenumber*500);
+      fs.readFile('./public/likedImage.csv', 'utf8', function(err, data2)
+      {
+        
+        if (err)
+        console.log(err);
+        let line = data2.split('\n');
+        var likeData =[];
+        line.map((x)=>{
+          var data = x.split(',');
+          if(data!= undefined && data!='"')
+          {
+            likeData.push(data[0]);
+          }
+        })
+
+       
+        var sentResp = [];
+        var obj ={};
+        responseData.map((x)=>{
+            obj={};
+            obj["value"] = x;
+            var get = x.split(',');
+            
+            if(likeData.indexOf(get[0])== -1){
+              obj["checked"] = false;
+            }
+            else
+            {
+              obj["checked"] = true;
+            }
+            sentResp.push(obj);
+        })
+       
+        res.json({ data: sentResp });
+
+      });
+      
   });
 });
 
@@ -51,44 +86,10 @@ router.post('/store',function(req,res){
           console.log('The CSV file was written successfully');
           res.send("deleted succesfully");
 });
-router.get('/cool', (req, res) => res.send(cool()))
-    //var idsToSearchFor = (req.body.imagesId || []).map((el)=> Number(el));
-    // fs.readFile('./public/data.csv', 'utf8', function(err, data)
-    //   {
-    //       if (err)
-    //       {
-    //           console.log(err);
-    //       }
-    //       let linesExceptFirst = data.split('\n');
-    //       let linesArr = linesExceptFirst.map(line=>line.split(','));
-    //       let output = linesArr.filter((x, index) =>idsToSearchFor.indexOf(Number(x[1])) == -1).join("\n");
-    //       //console.log(output);
-          
-    //       fs.writeFileSync('./public/likedImage.csv', output);
-    //       res.send("deleted succesfully");
-    //   });
 
-    });
 
+ });
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 console.log('Running at Port 3000');
-
-
-
-
-
-// var data = [];
-// for(var i = 0;i<10667;i++)
-// {
- 
-//     var x ={
-//         id:"inspirational_image_"+i,
-//         url:"https://s3.ap-south-1.amazonaws.com/couchfashiondressimages/inspiration+("+i+").jpg"
-//     }
-//     data.push(x);
-// }
-//   csvWriter  
-//     .writeRecords(data)
-//     .then(()=> console.log('The CSV file was written successfully'));
